a9157d9ff8366f840a9cab8ef4a984881d7f9e28
lizihan <339026163@qq.com>
2024-09-23 09:37:18 +0800

    add ik

---
+++ User/lib/ik_7dof_ofst_terminate.h
@@ -0,0 +1,28 @@
+/*
+ * ik_7dof_ofst_terminate.h
+ *
+ * Code generation for function 'ik_7dof_ofst_terminate'
+ *
+ */
+
+#ifndef IK_7DOF_OFST_TERMINATE_H
+#define IK_7DOF_OFST_TERMINATE_H
+
+/* Include files */
+#include "rtwtypes.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Function Declarations */
+extern void ik_7dof_ofst_terminate(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+/* End of code generation (ik_7dof_ofst_terminate.h) */
---
+++ User/lib/rtGetNaN.h
@@ -0,0 +1,25 @@
+/*
+ * rtGetNaN.h
+ *
+ * Code generation for function 'ik_7dof_ofst'
+ *
+ */
+
+#ifndef RTGETNAN_H
+#define RTGETNAN_H
+
+/* Include files */
+#include "rtwtypes.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern real_T rtGetNaN(void);
+extern real32_T rtGetNaNF(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+/* End of code generation (rtGetNaN.h) */
